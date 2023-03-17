a38e3b67c1be40574151da68624a25ba3023b16a
Ezra Reich <64613180+EZePiZy@users.noreply.github.com>
2023-03-17 18:19:25 +0000

    feat: mailman

--- Host/host.py
+++ Host/host.py
@@ -26,7 +26,11 @@ print(nios.get_info())
 
 SAMPLE_TIME = 0.001
 
-SERVER_URL = "http://54.90.46.38:8888/"
+
+################## CHANGE URL ####################
+SERVER_URL = "http://54.90.46.38:8888/" 
+SERVER_PING = "http://54.90.46.38:8888/ping" 
+##################################################
 
 isFirstMessage = True
 timestamp = datetime.datetime.now()
@@ -36,9 +40,8 @@ time_elapsed = 0
 
 
 while(True):
+  response = requests.get(SERVER_PING) # ALWAYS check server 
   time.sleep(SAMPLE_TIME)
-  # print("read: ", nios.read())
-
   b = nios.read().decode()
   if b != '':
     time_elapsed = 0
@@ -46,8 +49,6 @@ while(True):
         # get the initial start time
         timestamp = datetime.datetime.now() 
         isFirstMessage = False
-    # print("read: ", b)
-    
     b = b.split('\n')
     for val in b:
       if val != '':
@@ -63,27 +64,125 @@ while(True):
     # if no message has arrived add to time 
     time_elapsed += SAMPLE_TIME
 
-  if time_elapsed > 2:
-    if len(timestamps) > 0:
-      # pprint(timestamps)
-      # convert to unix 
-      timestamps = [ datetime.datetime.timestamp(x) for x in timestamps]
+  if time_elapsed > 2: #user stop tapping
+     '''
+      1. Always pinging the main server at SERVER_PING using the requests library
+        a. If user is sending data then stop pinging and wait for 5 seconds. 
+        b. Else GET data from server --> Status: Busy 
+      2. Save received data and convert to bytes 
+      3. Send data to FPGA using intel.write(data)
+      4. Set --> Status: Available 
+      5. Go back to step 1
+
+      check if stuff to send to server |OK|
+      check if stuff to send to user   |OK|
+      '''
+    # Send to user
+    if response.status_code == 200:
+      if not response.text:  # if no data is being sent --> server finished sending
+        data = requests.get(SERVER_URL).content  # assuming the data is obtained from this URL
+        json_str = json.dumps(data)
+        data_bytes = json_str.encode('utf-8')
+        intel.write(data_bytes)  # assuming intel.write() takes bytes as input
+    # Send to server 
+    if len(timestamps) > 0: 
+      timestamps = [ datetime.datetime.timestamp(x) for x in timestamps] # convert to unix 
       json_data = {
           "test":"test_data",
           "taps": timestamps
       }
-      saveTapsToFile(json_data, 'Thexquickxbrownxfoxxjumpsxoverxthexlazyxdog_fastEnd')
-      print("saved to file")
       try:
         response = requests.get(SERVER_URL, data = json.dumps(json_data))
         print("Sent data, received response:", response)
         pprint(json.dumps(json_data))
+        # Data Sent 
       except Exception:
-         print("Sending data failed :(", Exception)
-      time_elapsed = 0
-      timestamps = [] 
+        print("Sending data failed :(", Exception)
+        time_elapsed = 0
+        timestamps = []                   
+    else:
+      print(f'Server ping failed with status code {response.status_code}')
+
+
+
+ 
       
 
 
 
 
+
+
+
+
+
+
+
+# import datetime
+# import json
+# import sys
+# import time
+
+# import matplotlib.dates
+# import matplotlib.pyplot as plt
+# import requests
+
+# import intel_jtag_uart
+# import utils
+
+
+# def save_taps_to_file(taps, filename):
+#     with open(f'sample_data/{filename}.json', 'w', encoding='utf-8') as f:
+#         json.dump(taps, f)
+
+
+# def get_timestamps_from_buffer(buffer):
+#     timestamps = []
+#     for val in buffer.split('\n'):
+#         if val:
+#             tap, timedelta = val.split(":")
+#             print(tap, timedelta)
+#             timestamp = datetime.datetime.now() + datetime.timedelta(milliseconds=int(timedelta))
+#             timestamps.append(timestamp)
+#     return timestamps
+
+
+# def convert_timestamps_to_unix(timestamps):
+#     return [datetime.datetime.timestamp(x) for x in timestamps]
+
+
+# def send_data_to_fpga(intel, data):
+#     data_bytes = json.dumps(data).encode('utf-8')
+#     intel.write(data_bytes)
+
+
+# def run():
+#     try:
+#         nios = intel_jtag_uart.intel_jtag_uart()
+#         print(nios.get_info())
+#     except Exception as e:
+#         print(e)
+#         sys.exit(0)
+
+#     # Configuration
+#     SERVER_URL = "http://54.90.46.38:8888/"
+#     SERVER_PING = "http://54.90.46.38:8888/ping"
+#     SAMPLE_TIME = 0.001
+
+#     # State
+#     buffer = ''
+#     timestamps = []
+#     time_elapsed = 0
+#     status = 'Available'
+
+#     while True:
+#         time.sleep(SAMPLE_TIME)
+#         data = nios.read().decode()
+
+#         if data:
+#             time_elapsed = 0
+#             buffer += data
+
+#             if not timestamps:
+#                 timestamps = [datetime.datetime.now()]
+#             timestamps +=
